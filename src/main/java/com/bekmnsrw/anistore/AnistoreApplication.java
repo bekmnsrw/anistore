d121d2a4d37ee55468d70939c76d68b89ccf2c0d
bekmnsrw <ilya.bekmansurov@gmail.com>
2023-05-10 16:24:59 +0300

    initial commit

---
+++ src/main/java/com/bekmnsrw/anistore/AnistoreApplication.java
@@ -0,0 +1,13 @@
+package com.bekmnsrw.anistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnistoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnistoreApplication.class, args);
+    }
+
+}
