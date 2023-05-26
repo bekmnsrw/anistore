ad717332e423ac1874f68ae4647acaf22745cb34
bekmnsrw <ilya.bekmansurov@gmail.com>
2023-05-26 20:30:55 +0300

    another fix

--- src/main/java/com/bekmnsrw/anistore/controller/WelcomeController.java
+++ src/main/java/com/bekmnsrw/anistore/controller/WelcomeController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String getWelcomePage() { return "welcome"; }
 }
