6e6d0fd2241fd1e4ff74dc8f81c5461906c9b3bc
bekmnsrw <ilya.bekmansurov@gmail.com>
2023-05-26 19:07:22 +0300

    add schema.sql with products

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,59 @@
+INSERT INTO products
+    (id, category, description, image_url, price, title)
+VALUES (2, 'FIGURES', 'Small Itachi Uchiha''s figure. Add it to your collection!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/2.png?alt=media&token=1f8937c9-974b-4125-9935-752eba858024', 1200, 'Itachi Uchiha');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (3, 'FIGURES', 'Small Kakashi Hatake''s figure. Add it to your collection!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/3.png?alt=media&token=d6093239-3e38-427f-86bc-a5ecfb567fc6', 1500, 'Kakashi Hatake');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (4, 'MANGA', 'This is the story of a young ninja who dreams of becoming the Hokage', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/4.png?alt=media&token=d84047c9-6fde-4a21-9cf8-dbf472126303', 500, 'Naruto Chapter 1');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (5, 'SNACKS', 'Very tasty Choji Akimichi''s chips. Just try them!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/5.png?alt=media&token=337994f2-5452-4e40-b4be-71c60a33a654', 150, 'Choji''s Chips');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (6, 'FIGURES', 'Small Jiraiya''s figure. Add it to your collection!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/6.png?alt=media&token=b20491f1-f33b-40b0-a94a-d31ee8a9d775', 1000, 'Jiraiya');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (7, 'MANGA', 'This is the story of a young ninja who dreams of becoming the Hokage', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/7.png?alt=media&token=237ac0eb-7d28-4d32-8d81-c12787d93941', 500, 'Naruto Chapter 2');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (8, 'MANGA', 'This is the story of a young ninja who dreams of becoming the Hokage', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/8.png?alt=media&token=406ec603-81cd-4e6e-aa23-ec8a547421d9', 500, 'Naruto Chapter 3');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (9, 'MANGA', 'This is the story of a young ninja who dreams of becoming the Hokage', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/9.png?alt=media&token=182de1fb-5694-4c1f-95dd-a0754e4b407a', 500, 'Naruto Chapter 4');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (10, 'MANGA', 'This is the story of a young ninja who dreams of becoming the Hokage', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/10.png?alt=media&token=2e97ee1b-0716-45ea-a5c0-b945008e6bb2', 500, 'Naruto Chapter 5');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (11, 'MANGA', 'This is the story of a young ninja who dreams of becoming the Hokage', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/11.png?alt=media&token=2cd4c65d-6e0b-49c8-ac94-0908249cc47d', 500, 'Naruto Chapter 6');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (12, 'FIGURES', 'Small Naruto Uzumaki''s figure. Add it to your collection!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/12.png?alt=media&token=5c78d998-c20f-473a-8bd3-4a5b378e5104', 1000, 'Naruto Uzumaki');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (13, 'SNACKS', 'Very tasty naruto Uzumaki''s ramen. Just try it!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/13.png?alt=media&token=8943b940-27da-410f-97a3-ad6508ce0f98', 300, 'Naruto Uzumali''s ramen');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (14, 'FIGURES', 'Small Nedzuko Kamado''s figure. Add it to your collection!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/14.png?alt=media&token=f5ea643d-3f72-4c7e-8b17-026d4ba5d70d', 1300, 'Nedzuko Kamado');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (15, 'FIGURES', 'Small Yuji Itadori''s figure. Add it to your collection!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/15.png?alt=media&token=1e194064-bd39-419e-a809-c1cf980b6e0b', 1500, 'Yuji Itadori');
+
+INSERT INTO products
+(id, category, description, image_url, price, title)
+VALUES (16, 'SNACKS', 'Very tasty onigiri. Just try them!', 'https://firebasestorage.googleapis.com/v0/b/anistore-web.appspot.com/o/16.png?alt=media&token=9f0d4d9f-84af-469d-ad0a-3516be46f299', 100, 'Onigiri');
